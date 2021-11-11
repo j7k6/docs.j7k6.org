8710095b30850c2c6c09d4f8d929afc27949241e
j7k6 <mail@j7k6.org>
2021-11-11 09:54:49 +0100

    js

--- _includes/main.js
+++ _includes/main.js
@@ -13,18 +13,18 @@ document.querySelector('input[name=q]').addEventListener('keyup', function(e) {
   }
 
   if (q.replace(/\\/, '').length > 1) {
-    allItems.forEach((el) => el.style.display='none');
+    allItems.forEach(el => el.style.display='none');
 
     let queryWords = q.split(' ').map(word => `(?=.*${word}.*)`).join('');
     let matchItems = [...document.querySelectorAll('ul.index li')].filter(el => el.querySelector('a').innerText.match(new RegExp(`^(${queryWords}.+)`, 'i')));
 
-    matchItems.forEach((el) => el.style.display='block');
-    matchItems.forEach((el) => el.querySelector('a').innerHTML = el.querySelector('a').innerText.replace(new RegExp(q.split(' ').join('|'), 'gi'), match => `<strong>${match}</strong>`));
+    matchItems.forEach(el => el.style.display='block');
+    matchItems.forEach(el => el.querySelector('a').innerHTML = el.querySelector('a').innerText.replace(new RegExp(q.split(' ').join('|'), 'gi'), match => `<strong>${match}</strong>`));
   } else {
     [...document.querySelectorAll('ul.index li a strong')].forEach((el) => el.replaceWith(...el.childNodes));
 
-    allItems.forEach((el) => el.style.display='none');
-    favItems.forEach((el) => el.style.display='block');
+    allItems.forEach(el => el.style.display='none');
+    favItems.forEach(el => el.style.display='block');
   }
 });
 
