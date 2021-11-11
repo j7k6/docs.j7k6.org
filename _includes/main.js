ced3eab4ce7219bac9d27a60de974927c4c5feee
j7k6 <mail@j7k6.org>
2021-11-11 21:07:11 +0100

    js+css

--- _includes/main.js
+++ _includes/main.js
@@ -1,32 +1,48 @@
 'use strict';
 
-if (document.querySelector('input[name=q]') !== null) {
-  const allItems = [...document.querySelectorAll('ul.index li')];
+const inputElement = document.querySelector('input[name=q]');
+const allItems = [...document.querySelectorAll('ul.index li')];
 
-  document.querySelector('input[name=q]').addEventListener('keyup', function(e) {
-    let q; 
+if (inputElement !== null) {
+  let timeout;
+
+  inputElement.addEventListener('keyup', function(e) {
+    let q = '';
 
     if (e.keyCode === 27) {
-      q = document.querySelector('input[name=q]').value = '';
-    } else {
-      q = document.querySelector('input[name=q]').value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/\s\s+/g, ' ');
+      q = inputElement.value = '';
+    } else {  
+      q = inputElement.value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/(\s\s+|\s)/g, ' ');
+
+      let qc = q.replace(/\\/, '')
+
+      if (qc.length === 1) {
+        allItems.forEach(el => el.style.display='block');
+      }
+
+      if (qc.length > 1) {
+        clearTimeout(timeout);
+
+        timeout = setTimeout(function() {
+          allItems.forEach(el => el.style.display='none');
+
+          let queryWords = q.split(' ').map(word => `(?=.*${word}.*)`).join('');
+          let matchItems = allItems.filter(el => el.querySelector('a').innerText.match(new RegExp(`^(${queryWords}.+)`, 'i')));
+
+          matchItems.forEach(el => el.style.display='block');
+          matchItems.forEach(el => el.querySelector('a').innerHTML = el.querySelector('a').innerText.replace(new RegExp(q.split(' ').join('|'), 'gi'), match => `<strong>${match}</strong>`));
+        }, 100);
+      }
     }
 
-    if (q.replace(/\\/, '').length > 1) {
-      allItems.forEach(el => el.style.display='none');
-
-      let queryWords = q.split(' ').map(word => `(?=.*${word}.*)`).join('');
-      let matchItems = allItems.filter(el => el.querySelector('a').innerText.match(new RegExp(`^(${queryWords}.+)`, 'i')));
-
-      matchItems.forEach(el => el.style.display='block');
-      matchItems.forEach(el => el.querySelector('a').innerHTML = el.querySelector('a').innerText.replace(new RegExp(q.split(' ').join('|'), 'gi'), match => `<strong>${match}</strong>`));
-    } else {
+    if (q.length === 0) {
       [...document.querySelectorAll('ul.index li a strong')].forEach(el => el.replaceWith(...el.childNodes));
 
       allItems.forEach(el => el.style.display='none');
       allItems.filter(el => el.classList.contains('fav')).forEach(el => el.style.display='block');
     }
   });
-} else {
-  [...document.querySelectorAll('code')].forEach(el => el.innerHTML = el.innerText.replace(/<(\$[A-Z|_|0-9]+)>/g, match => `<span class="var">${match}</span>`));
 }
+
+// variable highlighter
+[...document.querySelectorAll('article code')].forEach(el => el.innerHTML = el.innerText.replace(/<([^$].*)>/gi, '&lt;$1&gt;').replace(/<\$([A-Z0-9_]+)>/g, match => `<span class="var">${match}</span>`));
---
+++ _posts/2021-11-11-firefox-enable-total-cookie-protection.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: "Enable Total Cookie Protection in Mozilla Firefox"
+---
+
+1. Open `about:config`.
+2. Set `network.cookie.cookieBehavior` = `5`.
+
+---
