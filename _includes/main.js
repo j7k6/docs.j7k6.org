0e9f85c38db5c98475702f0ed6592652de74dfa4
j7k6 <mail@j7k6.org>
2021-11-11 21:16:40 +0100

    js

--- _includes/main.js
+++ _includes/main.js
@@ -12,7 +12,7 @@ if (document.querySelector('input[name=q]') !== null) {
       q = document.querySelector('input[name=q]').value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/\s\s+/g, ' ');
     }
 
-    if (q.length.replace(/\\/, '') === 1) {
+    if (q.replace(/\\/, '').replace === 1) {
       allItems.forEach(el => el.style.display='block');
     }
 
